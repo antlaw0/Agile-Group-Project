f08e775f5a126017940681ec1bac701c6a2bea4e
Kayla Braun <go1018bx@go.minneapolis.edu>
2017-09-26 20:12:56 -0500

    flickr

---
+++ flickr.py
@@ -0,0 +1,64 @@
+from tkinter import *
+from flickrapi import FlickrAPI
+import urllib.request
+import requests
+import json
+import shutil
+from PIL import Image
+#used some code from Claras git hub repository https://github.com/minneapolis-edu/HelloFlikrGUI/blob/master/FlickrGUI.py
+
+class Flickr(Frame):
+
+
+    def __init__(self):
+
+        search = input('searching for')
+        Frame.__init__(self)
+        self.grid()
+
+        #Add a label to GUI
+        self._label = Label(self, text="Here are some pictures of " + search)
+        self._label.grid()
+        #code to call API from here http://joequery.me/code/flickr-api-image-search-python/
+        FLICKR_PUBLIC = '2996c5433c7c633978adb98583ac21fd'
+        FLICKR_SECRET = 'Ydbfbfec07e8f9c22'
+        flickr = FlickrAPI(FLICKR_PUBLIC, FLICKR_SECRET, format='parsed-json')
+        print ('n',flickr)
+        extras='url_sq,url_t,url_s,url_q,url_m,url_n,url_z,url_c,url_l,url_o'
+        results = flickr.photos.search(text=search, per_page=5, extras=extras)
+        print(results)
+        photos = results['photos']
+
+        from pprint import pprint
+        pprint(photos)
+
+
+        fetchPhotoURL = results['photos']['photo'][0]['url_c']
+        print(fetchPhotoURL)  # Again, just checking
+
+        # Reference: http://stackoverflow.com/questions/13137817/how-to-download-image-using-requests
+
+        catPicFileNameJpg = 'cat.jpg'
+        catPicFileGif = 'cat.gif'
+
+
+        resp = requests.get(fetchPhotoURL, stream=True)
+        with open(catPicFileNameJpg, 'wb') as out_file:
+            shutil.copyfileobj(resp.raw, out_file)
+        del resp
+
+        # Flickr returns a jpg. Tkinter displays gif. Use pillow to convert the JPG to GIF
+        # Reference https://pillow.readthedocs.org/handbook/tutorial.html
+        Image.open(catPicFileNameJpg).save(catPicFileGif)
+
+        # Add PictureImage to GUI
+        _catPic = PhotoImage(file=catPicFileGif)
+        _catPicLabel = Label(self, image=_catPic)
+        _catPicLabel.image = _catPic
+        _catPicLabel.grid()
+
+def main():
+
+    Flickr().mainloop()
+
+main()
